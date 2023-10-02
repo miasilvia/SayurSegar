edfdb652435d6333ee515b001eef49e964e9daf1
miasilvia <135966912+miasilvia@users.noreply.github.com>
2023-10-03 00:22:03 +0700

    installing express.js dan belajar buat method

--- server/index.js
+++ server/index.js
@@ -1,2 +1,29 @@
-const nama = "MIA SILVIAa"
-console.log(nama)
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.get("/product", (req, res) =>
+  res.send("Selamat datang di Website Sayur Segar")
+);
+
+app.get("/login", (req, res) => res.send("Ini login"));
+
+app.post("/post", (req, res) => {
+  console.log(req.body);
+  res.send("POST Request Called");
+});
+
+app.patch("/patch", (req, res) => {
+  console.log(req.body);
+  res.send("PATCH Request Called");
+});
+
+app.delete("/delete", (req, res) => {
+  console.log(req.body);
+  res.send("DELETE Request Called");
+});
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);

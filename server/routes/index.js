8df7a0884c229d70374e3d4ad8293f66b0b738d4
miasilvia <135966912+miasilvia@users.noreply.github.com>
2023-10-08 19:01:58 +0700

    menampilkan data produk

--- server/routes/index.js
+++ server/routes/index.js
@@ -14,12 +14,42 @@ routes.get("/greeting", (req, res) => {
 });
 
 routes.get("/data_produk", (req, res) => {
-  let dateNow = new Date();
+  // let dateNow = new Date();
   let dataProduk = {
-    timestamp: dateNow,
-    namaProduk: ["wortel", "buncis", "kubis"],
+    // timestamp: dateNow,
+    listProduk: [
+      {
+        nama_produk: "Wortel",
+        harga_produk: 12000,
+        stok_produk: 10,
+        satuan_produk: "1 kg",
+        foto_produk: "wortel.jpg",
+        deskripsi_produk: "ini adalah wortel",
+        diskon_produk: 2,
+      },
+      {
+        nama_produk: "Tomat",
+        harga_produk: 11000,
+        stok_produk: 10,
+        satuan_produk: "250 g",
+        foto_produk: "tomat.jpg",
+        deskripsi_produk: "ini adalah tomat",
+        diskon_produk: 2,
+      },
+      {
+        nama_produk: "Timun",
+        harga_produk: 5000,
+        stok_produk: 12,
+        satuan_produk: "250 g",
+        foto_produk: "timun.jpg",
+        deskripsi_produk: "ini adalah timun",
+        diskon_produk: 0,
+      },
+    ],
   };
-  res.send(dataProduk);
+  // res.send(dataProduk);
+
+  res.render("dataProduk", { dataProduk });
 });
 
 routes.get("../public/Sayuran.png", (req, res) => {
